c860c0329e7c678105e3231c708d98d0abcabcee
LeeSY0110 <112144874+LeeSY0110@users.noreply.github.com>
2023-09-15 09:55:34 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-customhost = "itp.c4qlbl5ajqxe.us-east-1.rds.amazonaws.com" 
+customhost = "itp.cpc3sv6dkw4o.us-east-1.rds.amazonaws.com" 
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "itp" 
-custombucket = "phoonwingyan-employee" 
-customregion = "us-east-1"
-
+custombucket = "sy-employee" 
+customregion = "us-east-1c"
